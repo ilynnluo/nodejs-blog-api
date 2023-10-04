159b8ff5f4a94656fba0d3dfc3077bde69bedecf
ilynnluo <60775749+ilynnluo@users.noreply.github.com>
2023-10-04 07:11:21 -0400

    > max the payload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParse = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -8,10 +9,13 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Methods", "*")
 	next()
 })
+// fixing "413 Request Entity Too Large" errors
+app.use(bodyParse.json({limit: "35mb", extended: true}))
+app.use(bodyParse.urlencoded({limit: "35mb", extended: true, parameterLimit: 50000}))
+
 app.use('/posts', require('./routes/api/posts'))
 app.get('/', (req, res) => {
   res.send('Hello NodeJs')
 })
-
 
 app.listen(port, () => {})
